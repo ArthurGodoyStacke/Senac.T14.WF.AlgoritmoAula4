7034eb38e622feb8f197ddcbd2c28e52c5eda78b
ArthurGodoyStacke <arthurgodoystacke@gmail.com>
2024-04-18 21:58:18 -0300

    Criada tela de conta

--- WinFormsAppAlgoritmo/Form1.Designer.cs
+++ WinFormsAppAlgoritmo/Form1.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.DarkViolet;
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Yu Gothic Medium", 13F);
+            groupBox1.Location = new Point(12, 57);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(743, 223);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cadastro de conta";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(363, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(178, 34);
+            label4.TabIndex = 7;
+            label4.Text = "Tipo da conta";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ScrollBar;
+            textBox4.Location = new Point(363, 160);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(351, 49);
+            textBox4.TabIndex = 6;
+            textBox4.Text = "Corrente";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(192, 34);
+            label3.TabIndex = 5;
+            label3.Text = "Saldo da conta";
+            label3.Click += label3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ScrollBar;
+            textBox3.Location = new Point(25, 160);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(332, 49);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "1000";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.Location = new Point(363, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(351, 49);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Arthur Godoy Stacke";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(363, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(203, 34);
+            label2.TabIndex = 2;
+            label2.Text = "Titular da conta";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(25, 79);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(332, 49);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "1234";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(220, 34);
+            label1.TabIndex = 0;
+            label1.Text = "Numero da conta";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label label3;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox4;
+        private Label label4;
     }
 }
--- WinFormsAppAlgoritmo/Form1.cs
+++ WinFormsAppAlgoritmo/Form1.cs
@@ -6,5 +6,30 @@ namespace WinFormsAppAlgoritmo
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
